6f801ed6f29f5d1f5ab4234b357a31851e63a18f
Vitalii Kulyk <vitcool@ukr.net>
2018-03-26 00:04:08 +0300

    CWA-2: rc0

--- src/components/CitySelector.js
+++ src/components/CitySelector.js
@@ -1,56 +1,88 @@
-import React, {Component} from 'react';
-import cities from './../mocks/cityList-min.json';
-import './../styles/CitySelector.css'
+import React, { Component } from "react";
+import cities from "./../mocks/cityList-min.json";
+import "./../styles/CitySelector.css";
 
-export default class CitySelector extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            searchValue: ""
+export default class CitySelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: "",
+      selectedCityId: 0
+    };
+  }
+  componentDidMount() {
+    //this.props.elementClick(700051);
+    //this.setState({searchValue: "nikopol"})
+  }
+  elementClick(cityId) {
+    this.setState({ selectedCityId: cityId });
+    this.props.elementClick(cityId);
+  }
+  onFilterChange(event) {
+    this.setState({ searchValue: event.target.value });
+  }
+  filterData(element) {
+    return element.name
+      .toLowerCase()
+      .includes(this.state.searchValue.toLowerCase());
+  }
+  showList() {
+    return (
+      <div className="collection">
+        {cities
+          .filter(element => this.filterData(element))
+          .map((element, index) => {
+            return (
+              <a
+                href="#"
+                className={"collection-item " + (this.state.selectedCityId == element.id ? "active" : "")}
+                key={element.id}
+                onClick={this.elementClick.bind(this, element.id)}
+              >
+                {element.name} - {element.country}
+              </a>
+            );
+          })}
+      </div>
+    );
+  }
+  render() {
+    const countries = this.getCountries();
+    return (
+      <div
+        className={
+          "city-selector-component " +
+          (this.props.isLeftSide ? "city-selector-component-left-side" : "")
         }
+      >
+        <div className={this.props.isLeftSide ? "half-city-selector-component" : ""}>
+          <div className="input-field">
+            <label htmlFor="city" className="validate" type="text">
+              City
+            </label>
+            <input
+              className="input-city-selector"
+              type="text"
+              onChange={this.onFilterChange.bind(this)}
+              id="city"
+            />
+          </div>
+          <div className="city-list">
+            {this.state.searchValue ? this.showList() : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  getCountries() {
+    var unique = {};
+    var distinct = [];
+    for (var i in cities) {
+      if (typeof unique[cities[i].country] == "undefined") {
+        distinct.push(cities[i].country);
+      }
+      unique[cities[i].country] = 0;
     }
-    elementClick(cityId){
-        this.props.elementClick(cityId)
-    }
-    onFilterChange(event) {
-        this.setState({searchValue: event.target.value});
-    }
-    filterData(element){
-        return element.name.toLowerCase().includes(this.state.searchValue.toLowerCase());
-    }
-    debounce(a,b,c){var d,e;return function(){function h(){d=null,c||(e=a.apply(f,g))}var f=this,g=arguments;return clearTimeout(d),d=setTimeout(h,b),c&&!d&&(e=a.apply(f,g)),e}}
-    showList(){
-        return (
-            <div className="collection">
-                {cities.filter((element) => this.filterData(element)).map((element, index) => {
-                    return (<a href="#" className="collection-item" key={element.id} onClick={this.elementClick.bind(this, element.id)}>{element.name} - {element.country}</a>)
-                })}
-            </div>
-        )
-    }
-    render(){
-        const countries = this.getCountries();
-        return(
-            <div className={"city-selector-component " + (this.props.isLeftSide ? "city-selector-component-left-side" : "")}>
-                <div class="input-field">
-                    <label for="city" class="validate" type="text">City</label>
-                    <input className="input-city-selector" type="text" onChange={this.onFilterChange.bind(this)} id="city"></input>
-                </div>
-                <div className="city-list">
-                    {this.state.searchValue ? this.showList() : null}
-                </div>
-            </div>
-        )
-    }
-    getCountries(){
-        var unique = {};
-        var distinct = [];
-        for( var i in cities ){
-         if( typeof(unique[cities[i].country]) == "undefined"){
-          distinct.push(cities[i].country);
-         }
-         unique[cities[i].country] = 0;
-        }
-        return distinct;
-    }
+    return distinct;
+  }
 }
--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,25 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./../styles/CurrentWeather.css";
 
-export default class CurrentWeather extends Component{
-    showWeatherData(cityData){
-        console.log(cityData)
-        return (
-            <div className="weather-info">
-                <div className="city-name">{cityData.name}</div>
-                <div className="temperature">temp {cityData.main.temp} &deg; C</div>
-                <div className="pressure">pressure {cityData.main.pressure} hPa</div>
-                <div className="humidity">humidity {cityData.main.humidity} %</div>
-                <div className="wind-speed">wind speed {cityData.wind.speed} m/s</div>
-                <div className="clouds">clouds {cityData.clouds.all} %</div>
-                <div className="coordinates"> coordinates: 
-                    long: {cityData.coord.lon} / lat : {cityData.coord.lat}
-                </div>
+import pressure from "./../img/pressure.png";
+import humidity from "./../img/humidity.png";
+import wind from "./../img/wind.png";
+import clouds from "./../img/clouds.png";
+
+export default class CurrentWeather extends Component {
+  hPaToMmHg(valHPa) {
+    //to const 0.75006375541921
+    return Math.round(0.75006375541921 * valHPa).toFixed(2);
+  }
+  showWeatherData(cityData) {
+    console.log(cityData);
+    return (
+      <div className="weather-info">
+        <div className="actual-weather-data">
+          <div className="main-info">
+            <div className="city-name">
+              {cityData.name}, {cityData.sys.country.toUpperCase()}
             </div>
-        )
-    }
-    render(){
-        return(
-            <div className="current-weather-component">{this.props.cityData ? this.showWeatherData(this.props.cityData) : 'Information will displayed here'}</div>
-        )
-    }
+            <div className="temperature">
+              {cityData.main.temp} <span>&deg;C</span>
+            </div>
+            <div className="weather-icon">
+              <img
+                src={
+                  "http://openweathermap.org/img/w/" +
+                  cityData.weather[0].icon +
+                  ".png"
+                }
+              />
+            </div>
+          </div>
+          <div className="additional-info">
+            <div className="pressure">
+              <img src={pressure} alt="pressure" />
+              <span>{this.hPaToMmHg(cityData.main.pressure)} mmHg </span>
+            </div>
+            <div className="humidity">
+              <img src={humidity} alt="humidity" />
+              <span>{cityData.main.humidity} % </span>
+            </div>
+            <div className="wind">
+                <img src={wind} alt="wind" /> 
+                <span>{cityData.wind.speed} m/s</span>
+            </div>
+            <div className="clouds">
+                <img src={clouds} alt="clouds" />
+                <span>{cityData.clouds.all} %</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div className="current-weather-component">
+        {this.props.cityData
+          ? this.showWeatherData(this.props.cityData)
+          : "Information will displayed here"}
+      </div>
+    );
+  }
 }
